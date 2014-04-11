fd8b82186d634674d40f479773e108b370f4d91f
chy111126@gmail.com <chy111126@gmail.com@e0a913ca-895f-ed08-c81d-f3862375ae42>
2014-04-11 10:55:54 +0000

    Compiled SoundTouch library, people w/o Android NDK should be able to launch the app now

---
+++ trunk/PaceKeeper/obj/local/armeabi-v7a/objs/soundtouch/soundtouch/source/SoundTouch/cpu_detect_x86.o.d
@@ -0,0 +1,20 @@
+obj/local/armeabi-v7a/objs/soundtouch/soundtouch/source/SoundTouch/cpu_detect_x86.o: \
+ jni/soundtouch/source/SoundTouch/cpu_detect_x86.cpp \
+ jni/soundtouch/source/SoundTouch/cpu_detect.h \
+ jni/soundtouch/include/STTypes.h \
+ /cygdrive/c/adt-bundle-windows-x86-20130729/android-ndk-r9c/platforms/android-18/arch-arm/usr/include/assert.h \
+ /cygdrive/c/adt-bundle-windows-x86-20130729/android-ndk-r9c/platforms/android-18/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/c/adt-bundle-windows-x86-20130729/android-ndk-r9c/platforms/android-18/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/c/adt-bundle-windows-x86-20130729/android-ndk-r9c/platforms/android-18/arch-arm/usr/include/android/api-level.h
+
+jni/soundtouch/source/SoundTouch/cpu_detect.h:
+
+jni/soundtouch/include/STTypes.h:
+
+/cygdrive/c/adt-bundle-windows-x86-20130729/android-ndk-r9c/platforms/android-18/arch-arm/usr/include/assert.h:
+
+/cygdrive/c/adt-bundle-windows-x86-20130729/android-ndk-r9c/platforms/android-18/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/c/adt-bundle-windows-x86-20130729/android-ndk-r9c/platforms/android-18/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/c/adt-bundle-windows-x86-20130729/android-ndk-r9c/platforms/android-18/arch-arm/usr/include/android/api-level.h:
