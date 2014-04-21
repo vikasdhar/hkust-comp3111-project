113998fdab8e9def869ee028a6cccc008c831f10
xccchung@gmail.com <xccchung@gmail.com@e0a913ca-895f-ed08-c81d-f3862375ae42>
2014-04-21 08:11:38 +0000

    modify on profile list so as to allow the apply button change the value of Usersetting class, minor changes on advanced settings

--- trunk/PaceKeeper/src/com/comp3111/pedometer/UserSettings.java
+++ trunk/PaceKeeper/src/com/comp3111/pedometer/UserSettings.java
@@ -1,16 +1,19 @@
 package com.comp3111.pedometer;
 
+import com.comp3111.local_database.DataBaseHelper;
+
 public class UserSettings {
 	// user information
-	public String userName = "Default";
-	public String userMail = "test@default.com";
-	public int age = 20;
-	public float height = 170;
-	public float weight = 68;
-	public int regionID = 0;
+	static public String userName = "Default";
+	static public String userMail = "test@default.com";
+	static public int age = 20;
+	static public float height = 170;
+	static public float weight = 68;
+	static public  int regionID = 0;
 	
 	// for pDefaultAverageStepDuration
-	public float walkSpeed = 5.0f;
-	public float jogSpeed = 4.5f;
-	public float runSpeed = 4.0f;
+	static public float walkSpeed = 5.0f;
+	static public float jogSpeed = 4.5f;
+	static public float runSpeed = 4.0f;
+	
 }
