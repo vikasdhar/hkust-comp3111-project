ffed8ce97c382c3d59ec089724b4477a6e824ed9
xccchung <xccchung@gmail.com>
2014-04-30 08:20:55 +0000

    Little modifications on setting to make it work in delete running histories

--- PaceKeeper/src/com/comp3111/pacekeeper/SettingActivity.java
+++ PaceKeeper/src/com/comp3111/pacekeeper/SettingActivity.java
@@ -1,7 +1,23 @@
 package com.comp3111.pacekeeper;
 
+import static com.comp3111.local_database.DataBaseConstants.ACH_TABLE;
+import static com.comp3111.local_database.DataBaseConstants.PRO_TABLE;
+import static com.comp3111.local_database.DataBaseConstants.PRO_USING;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.comp3111.local_database.DataBaseHelper;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Environment;
+import android.preference.DialogPreference;
 import android.preference.PreferenceActivity;
+import android.util.AttributeSet;
+import android.util.Log;
 
 public class SettingActivity extends PreferenceActivity {
 
@@ -9,5 +25,86 @@ public class SettingActivity extends PreferenceActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.setting);
+
 	}
+	
+	public static void deleteFiles() {
+
+		final String path = Environment.getExternalStorageDirectory()
+				.toString() + "/pacekeeper/";
+		File file = new File(path);
+
+		if (file.exists()) {
+			String deleteCmd = "rm -r " + path;
+			Runtime runtime = Runtime.getRuntime();
+			try {
+				runtime.exec(deleteCmd);
+			} catch (IOException e) {
+			}
+		}
+	}
+	
+
+
+
+
+}
+
+class CARDialogPreference extends DialogPreference {
+
+	public CARDialogPreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public void onClick(DialogInterface dialog, int which) {
+		Log.v("d", String.valueOf(which));
+		switch (which) {
+		case -1:
+			SettingActivity.deleteFiles();
+			break;
+		}
+	}
+	
+
+
+	@Override
+	protected void onDialogClosed(boolean positiveResult) {
+		super.onDialogClosed(positiveResult);
+		persistBoolean(positiveResult);
+	}
+
+}
+
+class RESETDialogPreference extends DialogPreference {
+
+	public RESETDialogPreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public void onClick(DialogInterface dialog, int which) {
+		Log.v("d", String.valueOf(which));
+		switch (which) {
+		case -1:
+			SettingActivity.deleteFiles();
+			break;
+		}
+	}
+
+
+	@Override
+	protected void onDialogClosed(boolean positiveResult) {
+		super.onDialogClosed(positiveResult);
+		persistBoolean(positiveResult);
+	}
+
+	public static void deletesqltable() {
+	//	DataBaseHelper dbhelper=new DataBaseHelper(getActivity());
+	//	SQLiteDatabase db = dbhelper.getWritableDatabase();
+		
+	//	db.execSQL("DROP TABLE IF EXISTS " + PRO_TABLE);
+	//	db.execSQL("DROP TABLE IF EXISTS " + PRO_USING);
+	//	db.execSQL("DROP TABLE IF EXISTS " + ACH_TABLE);
+//		dbhelper.onCreate(db);
+	}
+
 }
